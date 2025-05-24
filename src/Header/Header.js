28d79a81c82d5e67c30d0a49f0fc722b0d7b6495
Roman Dulal <dulalroman1@gmail.com>
2025-05-24 15:07:48 +0800

    navbar items

---
+++ src/Header/Header.js
@@ -0,0 +1,24 @@
+import { Component } from "react";
+import Navbar from "./Navbar";
+
+class Header extends Component{
+    render(){
+        return(
+            <div>
+                <header id="header">
+					
+						
+                    <Navbar>
+                        
+                    </Navbar>
+                       
+                    
+				</header>	
+
+            </div>
+        )
+    }
+
+}
+
+export default Header
